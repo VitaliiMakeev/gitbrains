d4ee88a28b7a62c4c40323e7c6b621f33e135720
Vitalii <vagontravki16@gmail.com>
2022-04-09 19:21:27 +0300

    reshenie_1

---
+++ ZD_1.py
@@ -0,0 +1,15 @@
+result = 15 * 3
+print(result)
+print(type(result))
+
+result1 = 15 / 3
+print(result1)
+print(type(result1))
+
+result2 = 15 // 2
+print(result2)
+print(type(result2))
+
+result3 = 15 ** 2
+print(result3)
+print(type(result3))
